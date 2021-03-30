e12e44240f84171afe40e4ce6c23e90d5c7e765e
Adis <adis.kubat@edu.fit.ba>
2021-03-30 13:12:34 +0200

    Front and Backend Validation
    Front and Backend Validation added on Registration and Login Form

---
+++ e-comm/ViewModels/Login/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_comm.ViewModels.Login
+{
+    public class IndexViewModel
+    {
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
